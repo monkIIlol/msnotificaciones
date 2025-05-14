b5fd0ee7a62b8012a4ddd9f651f0b3a22ee49a33
Cristian Urcullu <cr.urcullu@duocuc.cl>
2025-05-14 13:04:18 -0400

    first commit

---
+++ src/main/java/com/edutech/msnotificaciones/MsnotificacionesApplication.java
@@ -0,0 +1,13 @@
+package com.edutech.msnotificaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsnotificacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsnotificacionesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edutech/msnotificaciones/MsnotificacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edutech.msnotificaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsnotificacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
